5f6de1b2a61a154758faef0856bf96b6a59ec110
Shaan <shaanduss@gmail.com>
2023-12-11 03:04:12 +0800

    font changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Container mt={10}>
       <Heading as="h1">Shaan Dussoye</Heading>
-      <Box fontFamily="S1DK">Pursuing a BEng Computer Science at HKUST.</Box>
+      <Box fontFamily="Satoshi">Pursuing a BEng Computer Science at HKUST.</Box>
     </Container>
   )
 }
--- components/fonts.tsx
+++ components/fonts.tsx
@@ -6,13 +6,19 @@ const Fonts = () => (
       /* Outfit Bold */
       @font-face {
         font-family: 'Outfit Bold';
-        src: url('../public/fonts/Outfit-Bold.ttf') format('truetype');
+        src: url('./fonts/Outfit-Bold.ttf') format('truetype');
+      }
+
+      /* Outfit Regular */
+      @font-face {
+        font-family: 'Outfit';
+        src: url('./fonts/Outfit-Regular.ttf') format('truetype');
       }
 
       /* Satoshi */
       @font-face {
-        font-family: 'S1DK';
-        src: url('../public/fonts/Outfit-Bold.ttf') format('truetype');
+        font-family: 'Satoshi';
+        src: url('./fonts/Satoshi-Medium.otf') format('opentype');
       }
       `}
   />
